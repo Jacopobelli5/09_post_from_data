27bad9ec7d23cab282d3d9b028247f681eecc600
Jacopobelli5 <jacopo.b2016@gmail.com>
2025-04-14 21:36:40 +0100

    set up the body parser for post request

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const express = require ("express");
 const app = express();
 const port = 8081;
 
+// Set up the body parser
+app.use(express.urlencoded({extended: true}))
+
 // load the routes handlers
 const mainRoutes = require("./routes/main");
 app.use("/", mainRoutes);
--- routes/main.js
+++ routes/main.js
@@ -8,6 +8,8 @@ router.get("/about", (req, res) => res.render("about.ejs", {aboutTitle: "About",
 router.get("/contact", (req, res) => res.render("contact.ejs"))
 router.get("/search", (req, res) => res.render("search.ejs"))
 router.get("/search_result", (req, res) => res.send("You searched for " + req.query.search_text + " " + req.query.category ))
-
+// Handle the register GET and POST requests
+router.get("/register", (req, res) => res.render("/register.ejs"))
+router.post("/registered", (req, res) => res.send(req.body))
 // Export the router object
 module.exports = router
