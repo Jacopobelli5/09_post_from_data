4e8de418b1c337a77f597be41654c6ccc93f7f1d
Jacopobelli5 <jacopo.b2016@gmail.com>
2025-04-15 21:54:25 +0100

    added post methode for registered page with post request data

--- routes/main.js
+++ routes/main.js
@@ -9,7 +9,11 @@ router.get("/contact", (req, res) => res.render("contact.ejs"))
 router.get("/search", (req, res) => res.render("search.ejs"))
 router.get("/search_result", (req, res) => res.send("You searched for " + req.query.search_text + " " + req.query.category ))
 // Handle the register GET and POST requests
-router.get("/register", (req, res) => res.render("/register.ejs"))
-router.post("/registered", (req, res) => res.send(req.body))
+router.get("/register", (req,res) => {
+    res.render("register.ejs");
+    });
+    router.post("/registered", (req,res) => {
+    res.send("Hello "+ req.body.first + " "+ req.body.last +", you are now registered! We will send you an email to " + req.body.email); 
+    }); 
 // Export the router object
 module.exports = router
